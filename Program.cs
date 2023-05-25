58648b8c500e96b1e5599f707c3a9df240323e9a
PeYs92 <danielpejs@gmail.com>
2023-05-25 23:19:25 +0200

    relocation files

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheatherApp
+{
+    internal class Program
+    {
+        private static string city = string.Empty;
+
+        private static void Main(string[] args)
+        {
+            while (true)
+                ShowWeather(null);
+        }
+
+        private static void ShowWeather(dynamic weather)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("---------------");
+            Console.WriteLine("| Weather App |");
+            Console.WriteLine("---------------");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            if (weather == null)
+            {
+                Console.WriteLine("\n---------------------------------");
+                Console.WriteLine("| No data to display the weather! |");
+                Console.WriteLine("---------------------------------");
+            }
+            else
+            {
+                Console.WriteLine("\n-----------------------------------");
+                Console.WriteLine($"Current weather in [");
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(city);
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("]");
+
+                foreach (var day in weather.days)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Date -> {day.datetime}:");
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+                    Console.WriteLine($"    *Conditions -> {day.conditions}");
+                    Console.WriteLine($"    *Average temperature of day -> {ToCelsius(day.temp)} C");
+                    Console.WriteLine($"    *Min temperature of day -> {ToCelsius(day.tempmin)} C");
+                    Console.WriteLine($"    *Max temperature of day -> {ToCelsius(day.tempmax)} C");
+                    Console.WriteLine($"    *Humidity -> {day.humidity} %");
+                    Console.WriteLine($"    *Description -> {day.description}");
+                    Console.WriteLine($"    *Wind speed -> {day.windspeed} Km/h");
+                    Console.WriteLine($"    *Wind direction -> {day.winddir}° ");
+                    Console.WriteLine($"    *Pressure -> {day.pressure} Pa");
+
+                }
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("\n-----------------------------------");
+            }
+
+            Input();
+        }
+
+        private async static void LoadWeather()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Getting weather...");
+
+            Weather weather = new Weather();
+            weather.City = city;
+            weather.API = "F3CQY7DYEVNNPRC8FSVPNKPXK";
+
+            if (await weather.GetWeather())
+                ShowWeather(weather.CurrentWeather);
+            else
+                ShowWeather(null);
+        }
+
+        private static void Input()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write("\nPodaj miasto: ");
+            city = Console.ReadLine();
+
+            LoadWeather();
+        }
+
+        private static float ToCelsius(dynamic value)
+        {
+            float temp = ((float)value - 32f) * (5f / 9f);
+            return (float)Math.Round(temp, 1);
+        }
+    }
+}
---
+++ Weather.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WheatherApp
+{
+    public class Weather
+    {
+        private string _city;
+        private string _api;
+        private dynamic _currentWeather;
+        public string City
+        {
+            get => _city;
+            set
+            {
+                _city = value;
+            }
+        }
+        public string API
+        {
+            get => _api;
+            set
+            {
+                _api = value;
+            }
+        }
+        public dynamic CurrentWeather
+        {
+            get => _currentWeather;
+            set
+            {
+                _currentWeather = value;
+            }
+        }
+        public async Task<bool> GetWeather()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{_city}?key={_api}");
+
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            if (!response.IsSuccessStatusCode)
+                return false;
+
+            var body = await response.Content.ReadAsStringAsync();
+            dynamic json = JsonConvert.DeserializeObject(body);
+
+            _currentWeather = json;
+            return true;
+        }
+
+
+    }
+
+
+}
